a2cb1c665616ab77b823815c30f73215394adb34
Bharat Shah <akashche72@gmail.com>
2023-11-01 15:49:19 +0545

    using bean annotation for third party library

--- src/main/java/com/luv2code/springcodedemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcodedemo/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.luv2code.springcodedemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
@@ -7,9 +9,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CricketCoach implements  Coach {
+
     public CricketCoach() {
         System.out.println("In Constructor" + getClass().getSimpleName());
     }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In Constructor" + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanUpStuff() {
+        System.out.println("In Constructor" + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Lets play cricket";
